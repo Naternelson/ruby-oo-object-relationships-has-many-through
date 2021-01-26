6e7cbaf7b29fc4342c20b8924acad793a3e1d40a
Nate <7nater.nelson@gmail.com>
2021-01-26 13:32:13 -0700

    commit tree b665dd70e62e4716aaa17cdd4125a0afbff9dfd3

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,22 @@
 class Customer
-  
+    attr_accessor :name, :age
+    @@all = []
+    def initialize(name, age)
+        self.name = name
+        self.age = age
+        self.class.all << self
+    end
+    def self.all
+        @@all
+    end
+
+    def new_meal(waiter, total, tip)
+        Meal.new(waiter, self, total, tip)
+    end
+    def meals
+        Meal.all.select {|meal| meal.customer == self}
+    end
+    def waiters
+        self.meals.collect {|meal| meal.waiter}
+    end
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,27 @@
-class Waiter
+require 'pry'
 
+class Waiter
+    attr_accessor :name, :years_of_exp
+    @@all = []
+    def initialize(name, years_of_exp)
+        self.name = name
+        self.years_of_exp = years_of_exp
+        self.class.all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_meal(customer, total, tip)
+        Meal.new(self,customer,total,tip)
+    end
+    def meals
+        Meal.all.select {|meal| meal.waiter == self}
+    end
+    def best_tipper
+        m_sorted = meals.sort {|a,b| b.tip <=> a.tip}
+        # binding.pry
+        m_sorted[0].customer
+    end
 end
